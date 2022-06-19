bff7c9e1012016eeb87cd838f4345ff862845c24
ionut1195 <ionut_tomescu95@yahoo.com>
2022-06-19 16:42:33 +0200

    final version

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Clients from "../components/Clients";
 import Projects from "../components/Projects";
 import AddClientModal from "../components/AddClientModal";
+import AddProjectModal from "../components/AddProjectModal";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <>
     <div className="d-flex gap-3 mb-4">
       <AddClientModal/>
+      <AddProjectModal />
     </div>
 
       <Projects />
